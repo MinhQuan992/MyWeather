cd579e900203e35957cbbe5d43d247801ba497be
Võ Trần Minh Quân <deesatoshi@gmail.com>
2021-08-17 17:18:15 +0700

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     labelLocation.innerHTML = 'Loading...'
     const location = textLocation.value
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 alert(data.error + ' Please try again!')
